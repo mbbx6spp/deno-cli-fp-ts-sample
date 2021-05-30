aa33946b0131b84fdf03d2c3e4a1aed3cc9a74f8
Susan Potter <me@susanpotter.net>
2021-05-29 19:48:46 -0500

    Add README overview about how to use the Deno environment

---
+++ .repl.ts
@@ -0,0 +1,7 @@
+const fn        = await import('https://cdn.skypack.dev/fp-ts/function');
+const TE        = await import('https://cdn.skypack.dev/fp-ts/TaskEither');
+const IO        = await import('https://cdn.skypack.dev/fp-ts/IO');
+const Console   = await import('https://cdn.skypack.dev/fp-ts/Console');
+
+export { fn, TE, IO, Console };
+
